57a1d82610476876148cb4485d3ae33327619220
t-bittco <tasu.2331@gmail.com>
2020-01-30 23:58:20 +0900

    Basic認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-  # before_action :basic_auth
+  before_action :basic_auth, if: :production?
+  protect_from_forgery with: :exception
 
-  # private
+  private
 
-  # def basic_auth
-  #   authenticate_or_request_with_http_basic do |username, password|
-  #     username == 'admin' && password == '2222'
-  #   end
-  # end
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,3 +60,6 @@
 #     # password: "please use keys"
 #   }
 server '52.194.83.244', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
