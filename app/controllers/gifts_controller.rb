abc833f16fabdffdcd9fbb5ab1af31144dc95c8e
nyuristen <nyuristen@gmail.com>
2020-02-22 13:25:10 +0900

    Listing: 同時多数アップロード実装

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -6,4 +6,8 @@ class GiftsController < ApplicationController
   end
   def create
   end
+  private 
+    def product_params
+      params.require(:gift).permit(:name,images_attributes: [:name])
+end
 end
