22a0cc0de0989dc7049ee2f951d668ebd71564cc
nyuristen <nyuristen@gmail.com>
2020-02-24 01:35:56 +0900

    Gifts: セレクトボックスの表示

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -3,12 +3,20 @@ class GiftsController < ApplicationController
   end
   def new
     @parents = Category.all.order("id ASC").limit(13)
+# 親セレクトボックスの初期値(配列)
+    @category_parent_array = ["---------"]
+    # categoriesテーブルから親カテゴリーのみを抽出、配列に格納
+    Category.where(ancestry: nil).each do |parent|
+      @category_parent_array << parent.name
+    end
+    # # itemに紐づいていいる孫カテゴリーの親である子カテゴリが属している子カテゴリーの一覧を配列で取得
+    # @category_child_array = @gift.category.parent.parent.children
+    # # itemに紐づいていいる孫カテゴリーが属している孫カテゴリーの一覧を配列で取得
+    # @category_grandchild_array = @gift.category.parent.children
   end
   def create
-    # binding.pry
     @gift = Gift.create(gift_params)
     # @gift.images.build(gift_params)
-    # @gift.build.shipping_method(gift_params)
     # @gift.create!
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_140908) do
+ActiveRecord::Schema.define(version: 2020_02_23_163241) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "path", null: false
     t.string "name", null: false
     t.string "ancestry"
     t.datetime "created_at", null: false
