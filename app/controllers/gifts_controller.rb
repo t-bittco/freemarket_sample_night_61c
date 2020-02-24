69c204429effa6aeada50d5d12f51ad3aad590e0
nyuristen <nyuristen@gmail.com>
2020-02-24 15:22:21 +0900

    Gifts: controllerをgifts_controllerへ写した

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,5 +1,6 @@
 class GiftsController < ApplicationController
   def index
+    @gift = Gift.includes(:images).order("created_at DESC")
   end
   def new
     @parents = Category.all.order("id ASC").limit(13)
@@ -7,15 +8,18 @@ class GiftsController < ApplicationController
     @parents.each do |parent|
       @category_parent_array << parent.name
     end
+    binding.pry
+    @gift = Gift.new
+    @gift.images.build
   end
   def create
-    @gift = Gift.create(gift_params)
-    # @gift.images.build(gift_params)
-    # @gift.create!
+    @gift = Gift.new(gift_params)
+    @gift.images.new(gift_params)
+    @gift.save
   end
 
   private
   def gift_params
-    params.require(:gift).permit(:name, :discription, :shipping_charge, :how_to_ship, :sender_region, :days_to_ship, :state, :price)
+    params.require(:gift).permit(:name, :discription, :shipping_charge, :how_to_ship, :sender_region, :days_to_ship, :state, :price, images_attributes: [:name])
   end
 end
