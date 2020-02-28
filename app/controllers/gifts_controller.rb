ba4ed40edd41187449d358e82066bdbfba20279c
nyuristen <nyuristen@gmail.com>
2020-02-29 01:02:29 +0900

    Gifts: get_child_category(controller)

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -7,6 +7,12 @@ class GiftsController < ApplicationController
     Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
     end
+    def get_category_children
+      @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+    end
+    def get_category_grandchildren
+      @category_grandchildren = Category.find("#{params[:child_id]}").children
+    end
     @gift = Gift.new
     @gift.images.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'gifts#index'
   resource :gifts
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
 end
