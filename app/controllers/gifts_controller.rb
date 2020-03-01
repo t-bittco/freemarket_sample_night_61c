c81d7ceb62ac33b937dd6eaffd3d55981e76b553
nyuristen <nyuristen@gmail.com>
2020-03-01 13:36:08 +0900

    Gifts: categoryのajax通信による表示？実装

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -8,7 +8,7 @@ class GiftsController < ApplicationController
       @category_parent_array << parent.name
     end
     def get_category_children
-      @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+      @category_children = Category.find_by(name: "#{params[:parent_name]}").children
     end
     def get_category_grandchildren
       @category_grandchildren = Category.find("#{params[:child_id]}").children
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'gifts#index'
-  resource :gifts
+  resources :gifts do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
+  end
 end
