9c78bb3195c2f06e6cb0a5459e6eb190d4252567
nyuristen <nyuristen@gmail.com>
2020-03-09 14:42:31 +0900

    Gifts: controller_test

--- spec/controllers/gifts_controller_sepc.rb
+++ spec/controllers/gifts_controller_sepc.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 describe GiftsController do
+  describe 'GET #index' do
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+    # it "assigns the requested gifts to @gifts" do
+      
+    # end
+  end
   describe 'GET #new' do
     it "renders the :new template" do
       get :new
