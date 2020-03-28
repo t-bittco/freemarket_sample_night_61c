dc80956ec21316944a978d1ace307fc971e93ace
t-bittco <tasu.2331@gmail.com>
2020-03-28 11:17:37 +0900

    マイページ画面のマークアップ
    ・マイページトップ画面の実装
    ・ログアウト画面の実装
    ・クレジット登録画面
    ・プロフィール画面の実装
    ・本人確認画面の実装

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -13,12 +13,12 @@ class MypageController < ApplicationController
   end
 
   def card
-    @card = Card.find_by(user_id: current_user.id)
-    if @card.present?
-    Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    customer = Payjp::Customer.retrieve(@card.customer_id)
-    @default_card_information = customer.cards.retrieve(@card.card_id)
-    end
+    # @card = Card.find_by(user_id: current_user.id)
+    # if @card.present?
+    # Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+    # customer = Payjp::Customer.retrieve(@card.customer_id)
+    # @default_card_information = customer.cards.retrieve(@card.card_id)
+    # end
   end
 end
 
