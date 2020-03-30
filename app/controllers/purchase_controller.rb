216ec458ad8545c8572a620585c7ee484fc3bfb8
nyuristen <nyuristen@gmail.com>
2020-03-31 02:00:27 +0900

    Gift: index微調整
    indexに自身の出品した商品とこ購入された商品が表示されないようにする

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -5,7 +5,7 @@ class PurchaseController < ApplicationController
 
   def index
     if @card.blank?
-      redirect_to controller: "card", action: "new"
+      redirect_to controller: "cards", action: "new"
     else
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(@card.customer_id)
